54f03f33fd6d800d956d623ab7e4aaa22c8eab9d
tlanka <107880440+tlanka@users.noreply.github.com>
2022-08-25 15:06:26 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,7 +41,7 @@ git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 export GITHUB_USER="${GITHUB_ACTOR}"
 git add .
 git commit -m "Image tag in ${app_name}/values.yaml for ${app_name} with ${image_tag}"
-git push -u origin originize_repo
+git push -u origin ${git_repo}
 echo "==> Updated image tag in ${env}/${app_name}/values.yaml for ${app_name}"
 
 
